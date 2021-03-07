568a07c0bb28febc75a8b08c5e08e2df090c8954
Koray HALAT <korayhalat79@gmail.com>
2021-03-08 00:12:52 +0300

    angular_project

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title: string = 'northwind';        //tip belirlemek için , :string gibi kullanıyoruz.
+  user: string = 'Koray Halat';
+  
+
+}
+
